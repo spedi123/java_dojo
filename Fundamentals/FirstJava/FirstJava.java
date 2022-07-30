f4d07546a48790cf0c020bc0390d21dfb7ad5707
spedi123 <peteran8604@gmail.com>
2022-07-30 15:16:22 -0700

    first java assignment done

--- Fundamentals/FirstJava/FirstJava.java
+++ Fundamentals/FirstJava/FirstJava.java
@@ -1,7 +1,7 @@
 public class FirstJava {
     public static void main(String[] args) {
-        System.out.println("My name is Coding Dojo");
-        System.out.println("I am 100 years old");
-        System.out.println("My hometown is Burbank, CA");
+        System.out.println("My name is Peter An");
+        System.out.println("I am 36 years old");
+        System.out.println("My hometown is Gwang-Ju, South Korea");
     }
 }
