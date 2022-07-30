a4697dc9b3db24751054ef29f2861e4710768f22
spedi123 <peteran8604@gmail.com>
2022-07-30 15:07:30 -0700

    firstjava assignment done

--- Fundamentals/FirstJava.java
+++ Fundamentals/FirstJava.java
@@ -5,6 +5,5 @@ public class FirstJava {
         System.out.println("My name is Coding Dojo");
         System.out.println("I am 100 years old");
         System.out.println("My hometown is Burbank, CA");
-        System.out.println("git test");
     }
 }
