557c85dc857be1b00aed38f45953ef16e09e773c
spedi123 <peteran8604@gmail.com>
2022-08-04 13:56:54 -0700

    puzzling asignment done

--- Fundamentals/Puzzling/PuzzleJava.java
+++ Fundamentals/Puzzling/PuzzleJava.java
@@ -5,25 +5,27 @@ public class PuzzleJava {
     public ArrayList<Integer> getTenRolls() {
         ArrayList<Integer> tenRolls = new ArrayList<Integer>();
         Random randNum = new Random();
-
-        for (int i=0; i<10; i++) {
+        for (int i=0; i<11; i++) {
+            // 랜드넘을 10번 돌려서 나온숫자를 텐롤에 넣어주어야 함
             tenRolls.add(randNum.nextInt(21));
         }
-        return tenRolls;
+        return tenRolls;        
     }
-    
+
     public String getRandomLetter() {
+        
         Random randLetter = new Random();
 
-        String alphabetLetter = "abcdefghijklmnopqrstuvwxyz";
-        String[] alphabet = new String[26];
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String[] alphabetArray = new String[26];
+
         for (int i=0; i<26; i++) {
-            alphabet[i] = String.valueOf(alphabetLetter.charAt(i));
+            alphabetArray[i] = String.valueOf(alphabet.charAt(i));
         }
-        return alphabet[randLetter.nextInt(26)];
+        return alphabetArray[randLetter.nextInt(26)];
     }
 
-    public String generatePassword(){
+    public String generatePassword() {
         String password = "";
         for (int i=0; i<8; i++) {
             password += getRandomLetter();
@@ -32,11 +34,11 @@ public class PuzzleJava {
     }
 
     public ArrayList<String> getNewPasswordSet(int length) {
-        ArrayList<String> newPassword = new ArrayList<String>();
+        ArrayList<String> passwordSet = new ArrayList<String>();
 
         for (int i=0; i<length; i++) {
-            newPassword.add(generatePassword());
+            passwordSet.add(generatePassword());
         }
-        return newPassword;
+        return passwordSet;
     }
 }
--- Fundamentals/Puzzling/PuzzleTest.java
+++ Fundamentals/Puzzling/PuzzleTest.java
@@ -1,20 +1,20 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PuzzleTest {
-    public static void main(String[] args) {
+    
+	public static void main(String[] args) {
+		PuzzleJava generator = new PuzzleJava();
+		ArrayList<Integer> randomRolls = generator.getTenRolls();
+		System.out.println(randomRolls);
+		
+    	String randomLetter = generator.getRandomLetter();
+		System.out.println(randomLetter);
 
-        PuzzleJava generator = new PuzzleJava();
+		String randomPassword = generator.generatePassword();
+		System.out.println(randomPassword);
 
-        ArrayList<Integer> tenRolls = generator.getTenRolls();
-        System.out.println("Random numbers are : " + tenRolls);
-
-        String randomLetter = generator.getRandomLetter();
-        System.out.println("Random Letter is : " + randomLetter);
-
-        String password = generator.generatePassword();
-        System.out.println("Password is : " + password);
-
-        ArrayList<String> newPassword = generator.getNewPasswordSet(10);
-        System.out.println("Passwrd list are : " + newPassword);
-    }
+		ArrayList<String> passwordSet = generator.getNewPasswordSet(5);
+		System.out.println(passwordSet);
+	}
 }
