29ebda9ab845573a23fa7b261fb33700761b2230
spedi123 <peteran8604@gmail.com>
2022-08-06 22:49:05 -0700

    careSoft Interfaces assignment done

--- CaresoftInterface/src/com/caresoft/clinicapp/AdminUser.java
+++ CaresoftInterface/src/com/caresoft/clinicapp/AdminUser.java
@@ -1,25 +1,81 @@
 package com.caresoft.clinicapp;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class AdminUser extends User implements HIPAACompliantUser, HIPAACompliantAdmin {
 
-	@Override
-	public ArrayList<String> reportSecurityIncidents() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	private Integer employeeID;
+	private String role;
+	private ArrayList<String> securityIncidents;
 
+	// TO DO: Implement a constructor that takes an ID and a role
+	public AdminUser (Integer id, String role) {
+		super(id);
+		this.role = role;
+		this.securityIncidents = new ArrayList<String>();
+	}
+	
+	// TO DO: Implement HIPAACompliantUser!
 	@Override
 	public boolean assignPin(int pin) {
-		// TODO Auto-generated method stub
-		return false;
+		if (String.valueOf(pin).length() != 6) {
+			return false;
+		} else {
+			this.setPin(pin);
+			return true;
+		}
+	}
+	
+	@Override 
+	public boolean accessAuthorized(Integer confirmedAuthID) {
+		if (this.id.equals(confirmedAuthID)) {
+			return true;
+		} else {
+			this.authIncident();
+			return false;
+		}
+	}
+	// TO DO: Implement HIPAACompliantAdmin!
+	@Override 
+	public ArrayList<String> reportSecurityIncidents() {
+		return securityIncidents;
 	}
 
-	@Override
-	public boolean accessAuthorized(Integer confirmedAuthID) {
-		// TODO Auto-generated method stub
-		return false;
+	public void newIncident(String notes) {
+	    String report = String.format(
+	        "Datetime Submitted: %s \n,  Reported By ID: %s\n Notes: %s \n", 
+	        new Date(), this.id, notes
+	    );
+	    securityIncidents.add(report);
+	}
+	public void authIncident() {
+	    String report = String.format(
+	        "Datetime Submitted: %s \n,  ID: %s\n Notes: %s \n", 
+	        new Date(), this.id, "AUTHORIZATION ATTEMPT FAILED FOR THIS USER"
+	    );
+	    securityIncidents.add(report);
+	}
+	
+	// TO DO: Setters & Getters
+	public Integer getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(Integer employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public ArrayList<String> getSecurityIncidents() {
+		return securityIncidents;
+	}
+	public void setSecurityIncidents(ArrayList<String> securityIncidents) {
+		this.securityIncidents = securityIncidents;
 	}
 
 }
+
--- CaresoftInterface/src/com/caresoft/clinicapp/Physician.java
+++ CaresoftInterface/src/com/caresoft/clinicapp/Physician.java
@@ -4,8 +4,7 @@ import java.util.Date;
 import java.util.ArrayList;
 
 public class Physician extends User implements HIPAACompliantUser {
-//... imports class definition...
-    
+
     // Inside class:    
     private ArrayList<String> patientNotes;
 	
@@ -13,18 +12,8 @@ public class Physician extends User implements HIPAACompliantUser {
     public Physician(Integer id) {
     	super(id);
     }
-    // TO DO: Implement HIPAACompliantUser!
     
-	
-    public void newPatientNotes(String notes, String patientName, Date date) {
-        String report = String.format(
-            "Datetime Submitted: %s \n", date);
-        report += String.format("Reported By ID: %s\n", this.id);
-        report += String.format("Patient Name: %s\n", patientName);
-        report += String.format("Notes: %s \n", notes);
-        this.patientNotes.add(report);
-    }
-
+    // TO DO: Implement HIPAACompliantUser!
 	@Override
 	public boolean assignPin(int pin) {
 		// TODO Auto-generated method stub
@@ -40,6 +29,21 @@ public class Physician extends User implements HIPAACompliantUser {
 		return false;
 	}
 	
+    public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+	
     // TO DO: Setters & Getters
+	public ArrayList<String> getPatientNotes() {
+		return patientNotes;
+	}
 
+	public void setPatientNotes(ArrayList<String> patientNotes) {
+		this.patientNotes = patientNotes;
+	}
 }
