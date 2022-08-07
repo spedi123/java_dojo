d3702388e46c52c0837d6ec6c6a6e23430db7da8
spedi123 <peteran8604@gmail.com>
2022-08-06 17:41:16 -0700

    made controllers and index jsp file

---
+++ FruityLoops/src/test/java/com/spedi123/fruityloops/FruityLoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spedi123.fruityloops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityLoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
