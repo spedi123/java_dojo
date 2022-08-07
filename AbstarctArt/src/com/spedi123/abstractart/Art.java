2e3524c905b5614dc9da94992eb97465e349eb36
spedi123 <peteran8604@gmail.com>
2022-08-07 15:21:09 -0700

    abstract art assignment done

---
+++ AbstarctArt/src/com/spedi123/abstractart/Art.java
@@ -0,0 +1,40 @@
+package com.spedi123.abstractart;
+
+public abstract class Art {
+	private String title;
+	private String author;
+	private String description;
+	
+	public abstract void viewArt();
+	
+	public Art(String title, String author, String description) {
+		this.title = title;
+		this.author = author;
+		this.description = description;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
