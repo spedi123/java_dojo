e80fa0410d427a7c7b65f7f4c130cae201023275
spedi123 <peteran8604@gmail.com>
2022-08-09 08:09:24 -0700

    working on ninjagold game

---
+++ NinjaGoldgame/src/main/java/com/spedi123/ninjagoldgame/NinjaGoldgameApplication.java
@@ -0,0 +1,13 @@
+package com.spedi123.ninjagoldgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaGoldgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaGoldgameApplication.class, args);
+	}
+
+}
