4530aaeed65b671729c2a5c3fc2b93003f4603d7
spedi123 <peteran8604@gmail.com>
2022-08-10 00:09:47 -0700

    books api assignment done

---
+++ Book/src/test/java/com/spedi123/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spedi123.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
