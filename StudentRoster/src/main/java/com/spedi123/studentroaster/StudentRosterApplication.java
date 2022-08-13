c0abd7c3155d4c44249dd4f814d89a26dfa2c30d
spedi123 <peteran8604@gmail.com>
2022-08-13 12:18:24 -0700

    student roaster project initial setup

---
+++ StudentRoster/src/main/java/com/spedi123/studentroaster/StudentRosterApplication.java
@@ -0,0 +1,13 @@
+package com.spedi123.studentroaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRosterApplication.class, args);
+	}
+
+}
---
+++ StudentRoster/src/test/java/com/spedi123/studentroaster/StudentRosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spedi123.studentroaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
