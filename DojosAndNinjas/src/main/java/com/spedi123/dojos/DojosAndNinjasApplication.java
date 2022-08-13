2e6f5ee1ac780462abf643bbcba208269c1cdd6a
spedi123 <peteran8604@gmail.com>
2022-08-12 21:33:59 -0700

    spring boot set up

---
+++ DojosAndNinjas/src/main/java/com/spedi123/dojos/DojosAndNinjasApplication.java
@@ -0,0 +1,13 @@
+package com.spedi123.dojos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojosAndNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojosAndNinjasApplication.class, args);
+	}
+
+}
---
+++ DojosAndNinjas/src/test/java/com/spedi123/dojos/DojosAndNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spedi123.dojos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojosAndNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
