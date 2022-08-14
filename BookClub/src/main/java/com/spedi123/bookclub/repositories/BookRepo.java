e6c8918d70d5b094ad91d6abafa7f0ff5f7f1b93
spedi123 <peteran8604@gmail.com>
2022-08-14 16:12:04 -0700

    complete repositories

---
+++ BookClub/src/main/java/com/spedi123/bookclub/repositories/BookRepo.java
@@ -0,0 +1,14 @@
+package com.spedi123.bookclub.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spedi123.bookclub.models.Book;
+
+@Repository
+public interface BookRepo extends CrudRepository<Book, Long> {
+
+	List<Book> findAll();
+}
